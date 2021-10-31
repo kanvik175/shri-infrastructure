97e6cb825b5c91481d7eb881774b29c0c98c7549
rus <sr@trinitydigital.ru>
2021-10-31 23:51:11 +0300

    show git version

--- scripts/create_ticket.sh
+++ scripts/create_ticket.sh
@@ -2,8 +2,8 @@
 
 QUEUE_NAME=TMP
 
-TAG=$(git tag --sort=-creatordate | head -1)
-PREV_TAG=$(git tag --sort=-creatordate | sed -n '2 p')
+TAG=$(git tag | tail -1)
+PREV_TAG=$(git tag | sort -r | sed -n '2 p')
 HASH=$(git rev-parse HEAD | cut -c1-10)
 
 BUILD_NAME=$TAG-$HASH
@@ -11,6 +11,7 @@ SUMMARY="Релиз ${BUILD_NAME}"
 CHANGELOG=$(git log --pretty=format:"%h %s %an\n" $PREV_TAG..$TAG | tr -s "\n" " ")
 echo "prev tag ${PREV_TAG}"
 echo "tag ${TAG}"
+git --version
 
 DESCRIPTION="Версия релиза: ${TAG}\nВерсия пакета с релизом: ${BUILD_NAME}\n\n${CHANGELOG}"
 
