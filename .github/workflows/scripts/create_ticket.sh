bfe1fa31c9449b345a9372691cfeb35eeafcf7f9
rus <sr@trinitydigital.ru>
2021-10-31 18:00:59 +0300

    fix: release script

--- .github/workflows/scripts/create_ticket.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo $ORG_ID
-echo $APP_TOKEN
-echo 'done'
---
+++ scripts/create_ticket.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+QUEUE_NAME=TMP
+
+TAG=$(git tag --sort=-creatordate | head -1)
+PREV_TAG=$(git tag --sort=-creatordate | sed -n '2 p')
+HASH=$(git rev-parse HEAD | cut -c1-10)
+
+BUILD_NAME=$TAG-$HASH
+SUMMARY="Релиз ${BUILD_NAME}"
+CHANGELOG=$(git log $PREV_TAG..$TAG --pretty=format:"%h %s %an\n" | tr -s "\n" " ")
+
+DESCRIPTION="Версия релиза: ${TAG}\nВерсия пакета с релизом: ${BUILD_NAME}\n\n${CHANGELOG}"
+
+RESPONSE=$(curl -X  POST \
+-o /dev/null -s -w "%{http_code}\n" \
+-d '{"queue": "'"${QUEUE_NAME}"'", "summary": "'"${SUMMARY}"'", "description": "'"${DESCRIPTION}"'", "unique": "'"${BUILD_NAME}"'"}' \
+-H 'Content-Type: application-json' \
+-H 'X-Org-ID: '"$ORG_ID" \
+-H 'Authorization: OAuth '"$APP_TOKEN" \
+https://api.tracker.yandex.net/v2/issues/)
+
+echo $RESPONSE
+
+if [ $RESPONSE = "201" ]
+then
+  echo "Тикет создан"
+else
+  echo "Ошибка создания тикета"
+fi
