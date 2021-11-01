2eecba80af51238d747f7428702ebe8852888889
rus <sr@trinitydigital.ru>
2021-11-01 13:18:57 +0300

    test response code create comment

--- scripts/add_comment.sh
+++ scripts/add_comment.sh
@@ -5,17 +5,20 @@ COMMENT="{ \"text\": \"$1\" }"
 URL="https://api.tracker.yandex.net/v2/issues/${TASK_ID}/comments"
 
 CREATE_RESPONSE_CODE=$(curl -X  POST \
--o /dev/null -s -w "%{http_code}\n" \
 -d "$COMMENT" \
 -H 'Content-Type: application-json' \
 -H 'X-Org-ID: '"$ORG_ID" \
 -H 'Authorization: OAuth '"$APP_TOKEN" \
 $URL)
 
-if [ $CREATE_RESPONSE_CODE = '201' ]
-then
-  echo "comment \"$1\" is created"
-else
-  echo "comment is not created"
-  exit 1
-fi
+echo $CREATE_RESPONSE_CODE
+echo $TASK_ID
+echo $COMMENT
+
+# if [ $CREATE_RESPONSE_CODE = '201' ]
+# then
+#   echo "comment \"$1\" is created"
+# else
+#   echo "comment is not created"
+#   exit 1
+# fi
